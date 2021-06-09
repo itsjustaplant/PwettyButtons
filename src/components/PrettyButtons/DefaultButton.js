c83c25da39d8f21b46a829bfe6bcca2277b17d3b
itsjustaplant <alperensahin442@gmail.com>
2021-06-09 21:20:30 +0300

    feat: change danger color

--- src/components/PrettyButtons/DefaultButton.js
+++ src/components/PrettyButtons/DefaultButton.js
@@ -10,7 +10,7 @@ const BackgroundColors = {
     default: "#E0E0E0",
     primary: "#2962FF",
     secondary: "#455A64",
-    danger: "#D32F2F"
+    danger: "#D32F5F"
 };
 const HoverColors = {
     default: "#AEAEAE",
