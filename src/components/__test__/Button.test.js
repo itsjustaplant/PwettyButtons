37db7663da515dc0806a01667dcf6e3f35cfe645
itsjustaplant <alperensahin442@gmail.com>
2021-07-21 11:01:03 +0300

    test: added new test

--- src/components/__test__/Button.test.js
+++ src/components/__test__/Button.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable require-jsdoc, max-len */
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {cleanup, fireEvent, render} from '@testing-library/react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -11,45 +10,63 @@ afterEach(cleanup);
 Enzyme.configure({adapter: new Adapter()});
 
 describe('Button', () => {
-  const variant = 'default';
+  const defaultVariant = 'default';
+  const outlineVariant = 'outline';
+  const textVariant = 'text';
   it('should render without crash', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button/>, div);
+    render(<Button/>);
+  });
+  it('should render default button without crash', () => {
+    render(<Button variant={defaultVariant}/>);
+  });
+  it('should render outline button without crash', () => {
+    render(<Button variant={outlineVariant}/>);
+  });
+  it('should render text button without crash', () => {
+    render(<Button variant={textVariant}/>);
   });
   it('should show `text` prop', () => {
-    const wrapper = shallow(<Button variant={variant} text="text"/>);
+    const wrapper = shallow(<Button variant={defaultVariant} text="text"/>);
     expect(wrapper.props().children.props.children[1]).toEqual('text');
   });
   it('should show `startIcon` prop', () => {
-    const wrapper = shallow(<Button variant={variant} startIcon="lock"/>);
+    const wrapper = shallow(<Button variant={defaultVariant} startIcon="lock"/>);
     expect(wrapper.props().children.props.children[0].props.children).toEqual('lock');
   });
   it('should show `endIcon` prop', () => {
-    const wrapper = shallow(<Button variant={variant} endIcon="lock"/>);
+    const wrapper = shallow(<Button variant={defaultVariant} endIcon="lock"/>);
     expect(wrapper.props().children.props.children[2].props.children).toEqual('lock');
   });
   it('should pass `color` prop to child', () =>{
-    const wrapper = shallow(<Button variant={variant} color="danger"/>);
+    const wrapper = shallow(<Button variant={defaultVariant} color="danger"/>);
     expect(wrapper.props().children.props.specs.color).toEqual('danger');
   });
   it('should pass `size` prop to child', () => {
-    const wrapper = shallow(<Button variant={variant} size="lg"/>);
+    const wrapper = shallow(<Button variant={defaultVariant} size="lg"/>);
     expect(wrapper.props().children.props.specs.size).toEqual('lg');
   });
   it('should pass `disabled` prop to child', () => {
-    const wrapper = shallow(<Button variant={variant} disabled/>);
+    const wrapper = shallow(<Button variant={defaultVariant} disabled/>);
     expect(wrapper.props().children.props.specs.disabled);
   });
   it('should pass `disabledShadow` prop to child', () => {
-    const wrapper = shallow(<Button variant={variant} disableShadow/>);
+    const wrapper = shallow(<Button variant={defaultVariant} disableShadow/>);
     expect(wrapper.props().children.props.specs.disableShadow);
   });
   it('should trigger onclick function', () => {
     const click = jest.fn();
     const {getByTestId} = render(
-        <Button variant={variant} onClick={click}/>,
+        <Button variant={defaultVariant} onClick={click}/>,
     );
     fireEvent.click(getByTestId('default_button'));
     expect(click).toBeCalled();
+  });
+  it('should pass `size` prop to disabled default button', () => {
+    const wrapper = shallow(<Button variant={defaultVariant} disabled size='lg'/>);
+    expect(wrapper.props().children.props.specs.size).toEqual('lg');
+  });
+  it('should pass `size` prop to disabled text button', () => {
+    const wrapper = shallow(<Button variant={textVariant} disabled size='lg'/>);
+    expect(wrapper.props().children.props.specs.size).toEqual('lg');
   });
 });
